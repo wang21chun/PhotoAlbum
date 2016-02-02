4d7c8a63607a4a0b3fee98ee9d080506be393760
WangChun <wangchun@pindou.com>
2016-02-02 16:47:37 +0800

    add index navbar

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,40 +13,45 @@ module.exports = function(grunt) {
             }
         },
         copy: {
-        	main:{
-        		files: [{
-                expand: true,
-                cwd: 'node_modules/jquery/dist',
-                src: '*.min.js',
-                dest: 'public/js/',
-                flatten: true,
-                filter: 'isFile'
-            }, {
-                expand: true,
-                cwd: 'node_modules/bootstrap/dist/js',
-                src: '*.min.js',
-                dest: 'public/js/',
-                flatten: true,
-                filter: 'isFile'
-            },
-            {
-                expand: true,
-                cwd: 'node_modules/bootstrap/dist/css',
-                src: '*.min.css',
-                dest: 'public/style/',
-                flatten: true,
-                filter: 'isFile'
-            },
-            {
-                expand: true,
-                cwd: 'src/css/',
-                src: '*.css',
-                dest: 'public/style/',
-                flatten: true,
-                filter: 'isFile'
-            }],
-        	}
-            
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'node_modules/jquery/dist',
+                    src: '*.min.js',
+                    dest: 'public/js/',
+                    flatten: true,
+                    filter: 'isFile'
+                }, {
+                    expand: true,
+                    cwd: 'node_modules/bootstrap/dist/js',
+                    src: '*.min.js',
+                    dest: 'public/js/',
+                    flatten: true,
+                    filter: 'isFile'
+                }, {
+                    expand: true,
+                    cwd: 'node_modules/bootstrap/dist/css',
+                    src: '*.min.css',
+                    dest: 'public/style/',
+                    flatten: true,
+                    filter: 'isFile'
+                },
+                {
+                    expand: true,
+                    cwd: 'node_modules/bootstrap/dist/fonts',
+                    src: '**',
+                    dest: 'public/fonts',
+                    flatten: true,
+                    filter: 'isFile'
+                }],
+            }
+
+        },
+        cssmin: { //css文件压缩  
+            css: {
+                src: 'src/css/index.css', //将之前的all.css  
+                dest: 'public/style/index.min.css' //压缩  
+            }
         },
         clean: ["public/**"]
     });
@@ -55,8 +60,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-css');
 
     // 默认被执行的任务列表。
-    grunt.registerTask('default', ['copy', 'uglify']);
+    grunt.registerTask('default', ['copy', 'uglify','cssmin']);
 
 };
